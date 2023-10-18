5cb809dbbb851a918deec4e7c5cf5ffcbad94981
daniel <lordforcuadd12346@gmail.com>
2023-10-18 10:59:58 -0500

    pagina terminada sin ceo

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
+/*tiempo de carrusel*/
 $(".carousel").carousel({
   interval: 3000,
+});
+
+/*pnatalla de carga*/
+window.addEventListener("load", function () {
+  document.querySelector(".pantalla-carga").style.animation =
+    "fadeOut 1.5s ease-in-out forwards";
+  setTimeout(function () {
+    document.querySelector(".pantalla-carga").style.display = "none";
+    document.querySelector(".contenido").style.display = "block";
+    document.body.classList.remove("loading"); // Restablece el scroll
+  }, 1600);
 });
